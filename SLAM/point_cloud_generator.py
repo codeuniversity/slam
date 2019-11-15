5ff368ca8911b9f9f4b75f70d049b72a8b4af570
Katja Roth <rothk@telekom.de>
2019-11-15 17:25:54 +0100

    fix vector addition

--- SLAM/point_cloud_generator.py
+++ SLAM/point_cloud_generator.py
@@ -3,7 +3,7 @@
 def point_cloud_generator(robot_position,local_coordinates):
     global_coordinates = []
     for local_coordinate in local_coordinates:
-        local_coordinate += robot_position
+        local_coordinate = local_coordinate+robot_position
         global_coordinates.append(local_coordinate)
 
     return global_coordinates
