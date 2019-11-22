7c0c9d91a6ebc0829907b5d87a33e1300d0a0c19
Katja Roth <rothk@telekom.de>
2019-11-22 19:15:19 +0100

    fix landmark matching

--- SLAM/landmark_matcher.py
+++ SLAM/landmark_matcher.py
@@ -1,12 +1,11 @@
 stored_landmarks = []
 
-def match_landmarks(landmarks):
+def match_landmarks(landmarks_found):
     #compare vectors
     matched_landmarks = []
-    for landmark in landmarks:
-        for existing_landmark in stored_landmarks:
-            if landmark.AB==existing_landmark.AB and landmark.BC==landmark.BC:
-                matched_landmarks.append(landmark)
-            else:
-                stored_landmarks.append(landmark)
+    for landmark in landmarks_found:
+        if landmark in stored_landmarks:
+            matched_landmarks.append(landmark)
+        else:
+            stored_landmarks.append(landmark)
     return matched_landmarks
