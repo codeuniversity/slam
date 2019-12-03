9d3aa76345bd28ff07b2d8fbf3932ee6ce998ef1
Elias Wolf <Elias-Wolf@t-online.de>
2019-12-03 16:53:17 +0100

    Minor fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from SLAM.core import Point, Landmark
 from SLAM.landmark_extractor import extract_spike_landmarks
 from SLAM.landmark_matcher import match_landmarks
-from SLAM.position_evaluator import evaluate_position
+from SLAM.position_evaluator import evaluate_avg_position
 from SLAM.receiver import Receiver
 from multiprocessing import Queue
 from SLAM.globalizer import globalize_points, globalize_landmarks
@@ -11,7 +11,7 @@ from SLAM.sender import send_points, send_position
 def create_points(point_batch):
     points = []
     for p in point_batch:
-        points.append(SLAM.core.Point(p[0], p[1]))
+        points.append(Point(p[0], p[1]))
     return points
 
 
@@ -23,23 +23,25 @@ def main():
     receiver.start()
     new_landmarks=[]
     point_cloud=[]
-    for point_batch in queue.get():
+    while True:
+        point_batch = queue.get()
         point_cloud = create_points(point_batch) #local points
         found_landmarks = extract_spike_landmarks(point_cloud)   #local landmarks
         matched_landmarks,new_landmarks = match_landmarks(found_landmarks, stored_landmarks)
 
         if first_iteration:
-            robot_position= Point(None, None, x=0, y=0)
-            first_iteration=False
-        else:
-            robot_position = evaluate_position(matched_landmarks)
+            robot_position = Point(None, None, x=0, y=0)
+            first_iteration = False
+        elif len(matched_landmarks) > 0:
+            robot_position = evaluate_avg_position(matched_landmarks)
             new_landmarks = globalize_landmarks(robot_position, new_landmarks)
-            point_cloud = globalize_points(robot_position,point_cloud)
+            point_cloud = globalize_points(robot_position, point_cloud)
+        else:
+            continue
 
-        stored_landmarks.append(new_landmarks)
+        stored_landmarks.extend(new_landmarks)
         send_points(point_cloud)
         send_position(robot_position)
-
 
     return stored_landmarks, new_landmarks, point_cloud
 
