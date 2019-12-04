4059f16a679ada0c4b172c29aecea208c1479b52
Katja Roth <rothk@telekom.de>
2019-12-04 12:59:56 +0100

    remove pseudo code

--- SLAM/landmark_matcher.py
+++ SLAM/landmark_matcher.py
@@ -9,18 +9,9 @@ def match_landmarks(landmarks_found, stored_landmarks):
         for landmark in landmarks_found:
             for i in range(len(stored_landmarks)):
                 if landmark==stored_landmarks[i]:
+                    landmark.transfer_coordinates(stored_landmarks[i])
                     matched_landmarks.append(landmark)
-                    similar_landmarks.append(landmark)
                 else:
                     new_landmarks.append(landmark)
-<<<<<<< HEAD
-=======
-            if len(similar_landmarks)>1: more than one new matched landmark aka if a found landmark has multiple matched landmarks
-                landmark.transfer_coordinates(stored_landmarks[i])
-                ignore landmarks that are further away than a certain threshold
-                take average of all matched landmarks that are close to the
-                update landmark in stored_landmarks
-                extend matched_landmarks by
->>>>>>> extended draft
 
     return matched_landmarks, new_landmarks
